303fc8cf524c10e7469008520f63306cf3b6ab2a
Alexey Mochkin <alukardd@alukardd.org>
2014-10-13 08:16:14 +0400

    [WORK-470] Reading maps from attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -109,3 +109,4 @@ default['nginx']['proxy_read_timeout']      = nil
 default['nginx']['client_body_buffer_size'] = nil
 default['nginx']['client_max_body_size']    = nil
 default['nginx']['default']['modules']      = []
+default['nginx']['default']['maps']         = []
